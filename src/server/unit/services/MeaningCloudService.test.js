427eee46e82243903802cb3a2f835fcc302ba35c
Peter Okwara <peter.okwara@outlook.com>
2021-11-28 20:44:49 +0300

    SEt up jest to work well

--- src/server/unit/services/MeaningCloudService.test.js
+++ src/server/unit/services/MeaningCloudService.test.js
@@ -1,6 +1,5 @@
 describe("The meaningcloud service", () => {
   it("should fail if there is no response data from the api", async () => {
-
-    await expect().rejects.toThrow("")
+    await expect().rejects.toThrow("");
   });
 });
